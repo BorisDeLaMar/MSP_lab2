28b126216e531ede8f3e6cce0c4c68fbcc22d656
BorisDeLaMar <shumeyko.ilya@icloud.com>
2023-05-03 03:55:54 +0300

    r3

--- E.java
+++ E.java
@@ -35,8 +35,4 @@ public class E implements A, J, H {
     public int af() {
         return -1;
     }
-
-    public String nn() {
-        "".>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.;
-    }
 }
